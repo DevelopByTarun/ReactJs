496ab87915cc4b9ced288ae8f4bc1a85c3f2944e
TarunParve <gemini86.tarun@gmail.com>
2018-05-24 16:52:55 +0530

    Update appstore.js

--- ReactReduxLogin/src/models/store/appstore.js
+++ ReactReduxLogin/src/models/store/appstore.js
@@ -1,1 +1,8 @@
-rjherthethehehetht
+import {createStore,applyMiddleware} from 'redux';
+import {UserReducer} from '../reducers/userreducer';
+import thunk from 'redux-thunk';
+
+export const store = createStore(UserReducer,applyMiddleware(thunk));
+store.subscribe(()=>{
+    console.log("Store Up... ",store.getState());
+})
